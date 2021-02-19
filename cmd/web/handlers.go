de2d6383a7e948d9b829c722471aac377c2f5ee1
Daniil Maslov <i@dmaslov.dev>
2021-02-19 18:37:10 +0300

    Add new snippet template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/s0xzwasd/share-snippet/pkg/models"
+	"html/template"
 	"net/http"
 	"strconv"
 )
@@ -61,7 +62,22 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%v", s)
+	files := []string{
+		"./ui/html/show.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	err = ts.Execute(w, s)
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
