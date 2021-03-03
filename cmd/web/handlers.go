940a744ae50fbe470617c6493e85cc301a854eb9
Daniil Maslov <i@dmaslov.dev>
2021-03-03 17:37:41 +0300

    Add template entry

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -62,6 +62,8 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	data := &templateData{Snippet: s}
+
 	files := []string{
 		"./ui/html/show.page.tmpl",
 		"./ui/html/base.layout.tmpl",
@@ -74,7 +76,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.Execute(w, s)
+	err = ts.Execute(w, data)
 	if err != nil {
 		app.serverError(w, err)
 	}
---
+++ cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/s0xzwasd/share-snippet/pkg/models"
+
+type templateData struct {
+	Snippet *models.Snippet
+}
